3e46c62bf4df3e832b95e83db2359e9fe936c624
g-bgg <gbaggio90+2@gmail.com>
2026-07-08 15:59:11 +0200

    update banner

--- cmd/netassert/cli/root.go
+++ cmd/netassert/cli/root.go
@@ -21,9 +21,9 @@ var rootCmd = &cobra.Command{
 	Short: "NetAssert is a command line utility to test network connectivity between kubernetes objects",
 	Long: "NetAssert is a command line utility to test network connectivity between kubernetes objects.\n" +
 		"It currently supports Deployment, Pod, Statefulset and Daemonset.\nYou can check the traffic flow between these objects or from these " +
-		"objects to a remote host or an IP address.\n\nBuilt by ControlPlane https://control-plane.io",
+		"objects to a remote host or an IP address.\n\nMade with ❤ by https://control-plane.io/",
 
-	Version: fmt.Sprintf("\nBuilt by ControlPlane https://control-plane.io\n"+
+	Version: fmt.Sprintf("\nMade with ❤ by https://control-plane.io/\n"+
 		"Version: %s\nCommit Hash: %s\nBuild Date: %s\n",
 		version, gitHash, buildDate),
 }
